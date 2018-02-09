d2554edec97bb06c796b11e7f33871e56c7fd0f2
Kia Choi <sgchoi529@email.arizona.edu>
2018-02-09 10:16:27 -0700

    Initial Commit

---
+++ ex1/ex1.c
@@ -0,0 +1,20 @@
+int *alloc_1d(int cols)
+{
+	// !!! IMPLEMENT ME
+	// (solution is about 2 lines of code)
+}
+
+void alloc_1d_example(void)
+{
+	int *my_array = alloc_1d(12);
+
+	my_array[8] = 3490;
+
+	// The same array access could be made with pointer arithmetic:
+	*(my_array+8) = 3490;
+
+	// By the C spec, the array notation and pointer arithmetic notation
+	// are 100% equivalent.
+
+	printf("my_array[8] = %d\n", my_array[8]);
+}
---
+++ ex2/ex2.c
@@ -0,0 +1,21 @@
+int **alloc_2d(int rows, int cols)
+{
+	// !!! IMPLEMENT ME
+	// (solution is about 5 lines of code)
+}
+
+void alloc_2d_example(void)
+{
+	int **my_array = alloc_2d(5, 10);
+
+	// First array index is row, second is column:
+	my_array[2][3] = 3490;
+
+	// The same array access could be made with pointer arithmetic:
+	*(*(my_array+2)+3) = 3490;
+
+	// By the C spec, the array notation and pointer arithmetic notation
+	// are 100% equivalent.
+	
+	printf("my_array[2][3] = %d\n", my_array[2][3]);
+}
